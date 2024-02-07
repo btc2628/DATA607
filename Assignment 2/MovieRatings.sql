85765daf3517209418172e3d8f16def9684ccf83
Brandon Cunningham <37251868+btc2628@users.noreply.github.com>
2024-02-07 10:57:46 -0500

    Create MovieRatings.sql

---
+++ Assignment 2/MovieRatings.sql
@@ -0,0 +1,41 @@
+CREATE TABLE "MovieRatings" (
+	"MovieName"	TEXT,
+	"RaterName"	TEXT,
+	"Rating"	INTEGER
+)
+
+
+INSERT INTO MovieRatings ( MovieName,
+RaterName,
+Rating
+) VALUES
+('Oppenhimer', 'Robert', 10),
+    ('Oppenhimer', 'Trinity', 7),
+    ('Oppenhimer', 'Melanie', 8),
+    ('Oppenhimer', 'Zoe', 6),
+    ('Oppenhimer', 'Sydney', 7),
+    ('Hunger Games', 'Robert', 6),
+    ('Hunger Games', 'Trinity', 10),
+    ('Hunger Games', 'Melanie', 8),
+    ('Hunger Games', 'Zoe', 9),
+    ('Hunger Games', 'Sydney', 10),
+    ('Spider Man', 'Robert', 7),
+    ('Spider Man', 'Trinity', 10),
+    ('Spider Man', 'Melanie', 6),
+    ('Spider Man', 'Zoe', 10),
+    ('Spider Man', 'Sydney', 10),
+    ('John Wick', 'Robert', 10),
+    ('John Wick', 'Trinity', 9),
+    ('John Wick', 'Melanie', 10),
+    ('John Wick', 'Zoe', 8),
+    ('John Wick', 'Sydney', 6),
+    ('Dungeons & Dragons', 'Robert', 6),
+    ('Dungeons & Dragons', 'Trinity', 7),
+    ('Dungeons & Dragons', 'Melanie', 5),
+    ('Dungeons & Dragons', 'Zoe', 9),
+    ('Dungeons & Dragons', 'Sydney', 9),
+    ('Top Gun', 'Robert', 10),
+    ('Top Gun', 'Trinity', 10),
+    ('Top Gun', 'Melanie', 10),
+    ('Top Gun', 'Zoe', 8),
+    ('Top Gun', 'Sydney', 9)
